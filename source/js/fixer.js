afd0337a19398aef307bc1e6acf1524ac861f7ce
msramalho <19508417+msramalho@users.noreply.github.com>
2024-01-10 16:35:40 +0000

    typo fix for next run

--- source/js/fixer.js
+++ source/js/fixer.js
@@ -40,7 +40,7 @@ import HoverPopup from './hover-popup.js';
  * @property {URLResolver} url - Function that resolves the canonical URL for a timestamp.
  * @property {TimestampResolver} timestamp - Function that resolves the ISO timestamp for a node.
  * @property {AttachToResolver} attachTo - Function that resolves where the popup should attach to.
- * @property {MutationObserverResolver} observer - Function that is called upon DOM changes that can update the popup information, used when the HTML of the element hovered changes.
+ * @property {MutationObserverResolver} observe - Function that is called upon DOM changes that can update the popup information, used when the HTML of the element hovered changes.
  */
 
 /**
